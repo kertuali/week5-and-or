7af211a5386cc2ccc9c6936e6a6cc89ad82a24b9
Kertu Ali <kertu.greenberg@tktk.ee>
2023-10-02 20:07:18 +0200

    week5

---
+++ week5-and-or/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+//AND - &&
+
+//true && true --> true
+// true && false --> false
+//false && true --> false
+//false && false --> false
+
+//OR - || (PIPES)
+
+//true && true --> true
+// true && false --> true
+//false && true --> true
+//false && false --> false
